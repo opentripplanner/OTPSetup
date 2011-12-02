8fca6c81cfb962554de40b9779aaf71a51fc96ed
novalis <novalis@openplans.org>
2011-12-02 12:56:36 -0500

    more model cleanup

--- otpsetup/client/models.py
+++ otpsetup/client/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 STATES = (("running", "Running"),
           ("building", "Request not yet submitted"),
           ("pre_transload", "Getting GTFS"),
-          ("submitted", "Validating"),
+          ("submitted", "Waiting for approval"),
           ("rejected", "Rejected"),
           ("accepted", "Accepted, graph building"),
           )
@@ -17,16 +17,21 @@ class InstanceRequest(models.Model):
 
     user = models.ForeignKey(User)
     agency = models.CharField(max_length=40, blank=True)
-    comments = models.CharField(max_length=20000, null=True, blank=True)
+    comments = models.TextField(max_length=20000, null=True, blank=True)
     ip = models.CharField(max_length=15)
 
 class GtfsFile(models.Model):
     instance_request = models.ForeignKey(InstanceRequest)
     s3_key = models.CharField(max_length=200, null=True, db_index=True)
     transload_url = models.CharField(max_length=200, null=True)
-    validation_output = models.CharField(max_length=20000, null=True, blank=True)
+    validation_output = models.TextField(max_length=20000, null=True, blank=True)
 
 class InstanceRequestForm(ModelForm):
     class Meta:
         model = InstanceRequest
         fields = ('comments', 'agency')
+
+class AmazonInstance(models.Model):
+    ami_id = models.CharField(max_length=200)
+    machine_type = models.CharField(max_length=20)
+
