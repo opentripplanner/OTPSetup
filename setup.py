afb57f252554be8873b01442796f9c4d0f8c0948
Kevin Webb <kpwebb@gmail.com>
2011-12-07 16:02:00 -0500

    Changing setup.py to keep from installing wrong version of django-registration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     install_requires=[
       "Django==1.3.1",
-      "django-registration",
+#      "django-registration",  need to pull version 0.8 which is not currently available
       "kombu",
       "boto",
       "transitfeed"
